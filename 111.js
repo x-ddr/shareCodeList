457489ef4bcabcb861a2031bc5c73bb2b667bb08
小东 <49069586+x-dr@users.noreply.github.com>
2021-06-12 01:41:48 +0800

    Update 111.js

--- 111.js
+++ 111.js
@@ -657,12 +657,12 @@ cron "5 * * * *" script-path=https://raw.githubusercontent.com/yangtingxiao/Quan
               //await zoo_pk_assistGroup()
               //if (data.data.result.homeMainInfo.raiseInfo.buttonStatus === 1 )
               if (parseInt(data.data.result.homeMainInfo.raiseInfo.totalScore) >= parseInt(data.data.result.homeMainInfo.raiseInfo.nextLevelScore) ) await zoo_raise(1000)
-              await zoo_getHomeData('ZXTKT0225KkcRhdI8AHeJx_8x_9cIgFjRWn6-7zx55awQ');
-              console.log("pk1")
-              await zoo_getHomeData('ZXTKT0225KkcRU0Y8FXeJBL2xqFbcQFjRWn6-7zx55awQ');
-              console.log("pk2")
-              await zoo_getHomeData('ZXTKT011a33MlJa1ogsFjRWn6-7zx55awQ');
-              console.log("pk3")
+            //   await zoo_getHomeData('ZXTKT0225KkcRhdI8AHeJx_8x_9cIgFjRWn6-7zx55awQ');
+            //   console.log("pk1")
+            //   await zoo_getHomeData('ZXTKT0225KkcRU0Y8FXeJBL2xqFbcQFjRWn6-7zx55awQ');
+            //   console.log("pk2")
+            //   await zoo_getHomeData('ZXTKT011a33MlJa1ogsFjRWn6-7zx55awQ');
+            //   console.log("pk3")
             //   await zoo_getTaskDetail()
             //   await zoo_getTaskDetail("","app")
             } else {
